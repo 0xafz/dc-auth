3423bce14bae2c040fa7e4f2c63de6654fd782fc
Afroz <0xafz.dev@gmail.com>
2025-02-02 18:35:55 +0530

    Update

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export default {
   subtitle: `Web Developer`,
   description: `This is demo app which demostrates various social authentication providers.`,
   author: `Afroz Mohammad`,
-  userName: `afroz_0000`, //default for all
-  twitterUserName: `afroz_0000`,
+  userName: `af4oz`, //default for all
+  twitterUserName: `af4oz`,
   baseUrl: `https://auths.vercel.app/`,
   image: `https://i.imgur.com/8PPjpAD.png`,
-  codeUrl: `https://github.com/zkindest/dc-auth`,
+  codeUrl: `https://github.com/af4oz/dc-auth`,
 }
